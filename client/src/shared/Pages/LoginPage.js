4fafe95573a0b33e4a00b1d4a1a9c48a3260c3a2
stevorated <garbers8@gmail.com>
2019-05-31 12:04:04 +0300

    refactored redundant GQL out of component

--- client/src/shared/Pages/LoginPage.js
+++ client/src/shared/Pages/LoginPage.js
@@ -4,18 +4,9 @@ import { LoginForm, Loading } from '../Components'
 import gql from 'graphql-tag'
 import forceLoggedIn from '../HOC/forceLoggedIn'
 
-const LOGIN_USER = gql`
-  mutation signIn(
-$email: String!
-$password: String!
-) { signIn(email: $email, password: $password ) {id} }
-`
-
 function LoginPage () {
   return (
-    <div>
-      <LoginForm login={LOGIN_USER} />
-    </div>
+      <LoginForm />
   )
 }
 
--- client/src/shared/Pages/RegisterPage.js
+++ client/src/shared/Pages/RegisterPage.js
@@ -4,29 +4,9 @@ import { RegisterForm, Loading } from '../Components'
 import gql from 'graphql-tag'
 import forceLoggedIn from '../HOC/forceLoggedIn'
 
-const REGISTER_USER = gql`
- mutation ($fname: String!, $lname: String!, $username: String!, $email: String!, $password: String!) {
-  signUp (
-    fname: $fname,
-    lname: $lname,
-    username: $username,
-    email: $email,
-    password: $password
-  ) {
-    id
-    email
-    fname
-    lname
-    username
-  }
-}
-`
-
 function RegisterPage () {
   return (
-    <div>
-      <RegisterForm register={REGISTER_USER} />
-    </div>
+      <RegisterForm/>
   )
 }
 
